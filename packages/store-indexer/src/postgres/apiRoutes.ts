a47d29d5f529e2809e1dd3f3448e20169e86016d
hubur <hubur@asphodel.io>
2026-04-24 14:06:26 +0200

    Remove cache from GET api/logs

--- packages/store-indexer/src/postgres/apiRoutes.ts
+++ packages/store-indexer/src/postgres/apiRoutes.ts
@@ -62,20 +62,6 @@ export function apiRoutes(database: Sql): Middleware {
       const blockNumber = records[0].chainBlockNumber;
       ctx.status = 200;
 
-      // max age is set to several multiples of the uncached response time (currently ~10s, but using 60s for wiggle room) to ensure only ~one origin request at a time
-      // and stale-while-revalidate below means that the cache is refreshed under the hood while still responding fast (cached)
-      const maxAgeSeconds = 60 * 5;
-      // we set stale-while-revalidate to the time elapsed by the number of blocks we can fetch from the RPC in the same amount of time as an uncached response
-      // meaning it would take ~the same about of time to get an uncached response from the origin as it would to catch up from the currently cached response
-      // if an uncached response takes ~10 seconds, we have ~10s to catch up, so let's say we can do enough RPC calls to fetch 4000 blocks
-      // with a block per 2 seconds, that means we can serve a stale/cached response for 8000 seconds before we should require the response be returned by the origin
-      const staleWhileRevalidateSeconds = 4000 * 2;
-
-      ctx.set(
-        "Cache-Control",
-        `public, max-age=${maxAgeSeconds}, stale-while-revalidate=${staleWhileRevalidateSeconds}`,
-      );
-
       ctx.set("Content-Type", "application/json");
       log.info("response ok", { blockNumber: blockNumber.toString(), logs: logs.length });
       ctx.body = JSON.stringify({ blockNumber, logs });
